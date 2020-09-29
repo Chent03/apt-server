db84dd798942641cd4216402011404fedd637100
chent03 <tc56007n@pace.edu>
2020-09-29 11:00:35 -0400

    revert

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func main() {
 	r.HandleFunc("/api/review", createReview).Methods("POST")
 
 	handler := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
+		AllowedOrigins:   []string{"http://localhost:3000"},
 		AllowCredentials: true,
 	}).Handler(r)
 
