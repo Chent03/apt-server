73f2c557f6596c5111bd771509ed72c1f2a09138
chent03 <tc56007n@pace.edu>
2020-09-30 19:29:47 -0400

    try

--- controllers/users.go
+++ controllers/users.go
@@ -127,7 +127,7 @@ func (u *Users) signIn(w http.ResponseWriter, user *models.User) error {
 	cookie := http.Cookie{
 		Name:     "remember_token",
 		Value:    user.Remember,
-		HttpOnly: true,
+		HttpOnly: false,
 	}
 	http.SetCookie(w, &cookie)
 	return nil
